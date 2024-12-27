437eaa72c255cfd5061903a58b4fc8571db9fe4c
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2024-12-27 17:23:44 +0500

    test1

--- router/users.routes.js
+++ router/users.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
-
-const mainRouter = express.Router();
+import path from "path";
+export const mainRouter = express.Router();
 
 mainRouter.get("/", (req, res) => {
     try {
-        return  res.send("Hello World!");
-        
+        return res.sendFile(path.join(process.cwd() + "views", "index.html"));
+
     } catch (error) {
         console.log(error);
-        return  res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     }
 });
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
+import { mainRouter } from "./router/users.routes.js";
+
 config()
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+app.use(express.static("views"));
 
-app.use("/api", mainRouter);
+app.use("/", mainRouter);
 
 
 app.listen(PORT, () => {
